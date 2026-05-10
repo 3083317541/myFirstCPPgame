e10dcf4c126c18da7ec68a6fc08e5aa737fb0622
3083317541 <3083317541@qq.com>
2026-05-10 20:16:21 +0800

    GameMap

--- src/gameLayer/gameMain.cpp
+++ src/gameLayer/gameMain.cpp
@@ -2,11 +2,12 @@
 #include "gameMain.h"
 #include <asserts.h>
 #include <assetManager.h>
+#include <gameMap.h>
 
 struct GameDate
 {
 
-	Texture dirtTexture;
+	GameMap gameMap;
 
 }gameData;
 
@@ -16,8 +17,14 @@ bool initGame()
 {
 
 	assetManager.loadAll();
+	
+	gameData.gameMap.create(30, 10);
 
-	gameData.dirtTexture = LoadTexture(RESOURCES_PATH "dirt.png");
+	gameData.gameMap.getBlockUnsafe(0, 0).type = Block::dirt;
+	gameData.gameMap.getBlockUnsafe(1, 1).type = Block::dirt;
+	gameData.gameMap.getBlockUnsafe(2, 2).type = Block::dirt;
+	gameData.gameMap.getBlockUnsafe(3, 3).type = Block::dirt;
+	gameData.gameMap.getBlockUnsafe(4, 4).type = Block::dirt;
 
 	return true;
 }
@@ -27,8 +34,32 @@ bool updateGame()
 	float deltaTime = GetFrameTime();
 	if (deltaTime > 1.f / 5) { deltaTime = 1 / 5.f; }
 
-	DrawTexturePro(gameData.dirtTexture, { 0, 0, (float)gameData.dirtTexture.width, (float)gameData.dirtTexture.height },
-		{ 50, 50, 100, 100 }, {}, 0, WHITE);
+	ClearBackground({ 75, 75, 150, 255 });
+
+	for (int y = 0; y < gameData.gameMap.h; y++)
+		for(int x = 0; x < gameData.gameMap.w; x++)
+		{
+			
+			auto& b = gameData.gameMap.getBlockUnsafe(x, y);
+
+			if (b.type != Block::air)
+			{
+				float size = 32;
+				float posX = x * size;
+				float posY = y * size;
+
+				DrawTexturePro(
+					assetManager.dirt,
+					Rectangle{ 0.f, 0.f, (float)assetManager.dirt.width, (float)assetManager.dirt.height },
+					{posX, posY, size, size},
+					{ 0, 0 },
+					0.0f,
+					WHITE
+				);
+
+			}
+
+		}
 
 
 	return true;
